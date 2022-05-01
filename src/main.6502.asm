df0d4f36526dd4e8e92679c9adfcc6592e948db0
Illia Storozhuk <csharp.storozhuk@gmail.com>
2022-05-01 21:08:00 +0300

    Display first sprite

--- src/main.6502.asm
+++ src/main.6502.asm
@@ -7,6 +7,10 @@
 .endproc
 
 .proc nmi_handler
+  LDA #$00
+  STA OAMADDR
+  LDA #$02
+  STA OAMDMA
   RTI
 .endproc
 
@@ -14,6 +18,7 @@
 
 .export main
 .proc main
+  ; write a pallete
   LDX PPUSTATUS
   LDX #$3f
   STX PPUADDR
@@ -21,7 +26,27 @@
   STX PPUADDR
   LDA #$29
   STA PPUDATA
-  LDA #%00011110
+  LDA #$19
+  STA PPUDATA
+  LDA #$09
+  STA PPUDATA
+  LDA #$0f
+  STA PPUDATA
+  ; write sprite data
+  LDA #$70
+  STA $0200       ; Y-coord of first sprite
+  LDA #$05
+  STA $0201       ; tile number of first sprite
+  LDA #$00
+  STA $0202       ; attributes of first sprite
+  LDA #$80
+  STA $0203       ; X-coord of first sprite
+vblankvait:       ; wait for another vblank before continuing
+  BIT PPUSTATUS
+  BPL vblankvait
+  LDA #%10010000  ; turn on NMIs, prites use first pattern table
+  STA PPUCTRL
+  LDA #%00011110  ; turn on screen
   STA PPUMASK
 forever:
   JMP forever
@@ -31,4 +56,4 @@ forever:
 .addr nmi_handler, reset_handler, irq_handler
 
 .segment "CHR"
-.res 8192
+.incbin "content/graphics.chr"
